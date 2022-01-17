31a04133959a9c3d705f0a91844c0868242fbd4d
emmanuel <gbengacode>
2022-01-17 10:09:41 +0100

    Add Navbar to app.js

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import { Routes, Route } from 'react-router-dom';
 import BookList from './pages/BookList';
 import Categories from './pages/Categories';
 import './asset/main.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-
-    <Routes>
-      <Route path="/" element={<BookList />} />
-      <Route path="/categories" element={<Categories />} />
-    </Routes>
-
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<BookList />} />
+        <Route path="/categories" element={<Categories />} />
+      </Routes>
+    </>
   );
 }
 
