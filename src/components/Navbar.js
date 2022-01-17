7fd8f7c616ccc586c8217346c76df32005dabb27
emmanuel <gbengacode>
2022-01-17 10:09:15 +0100

    Add Link to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => (
   <>
     <nav>
       <ul>
-        <li>Home</li>
-        <li>BookList</li>
-        <li>Category</li>
+        <li><Link to="/">Books</Link></li>
+        <li><Link to="/categories">Categories</Link></li>
       </ul>
     </nav>
   </>
