e43369f70e99878c7d85579a098883f8244c4c50
emmanuel <gbengacode>
2022-01-18 13:31:52 +0100

    Refactor code to work with Api

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,18 +1,62 @@
+/* eslint-disable consistent-return */
+import Axios from 'axios';
 // actions
 const ADD_BOOK = 'bookStore/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookStore/books/REMOVE_BOOK';
+const FETCH_BOOK = 'bookStore/books/FETCH_BOOK';
 
 // action creator
 
-export const addBook = (payload) => ({
+const addBook = (payload) => ({
   type: ADD_BOOK,
   payload,
 });
 
-export const removeBook = (payload) => ({
+const removeBook = (payload) => ({
   type: REMOVE_BOOK,
   payload,
 });
+
+const fetchBook = (payload) => ({
+  type: FETCH_BOOK,
+  payload,
+});
+// asychrous function with side Effect
+
+export const addBookApi = (formData) => async (dispatch) => {
+  try {
+    await Axios.post('https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/oF2DlchTxHbEjx4GHO3I/books', formData);
+    dispatch(addBook(formData));
+  } catch (error) {
+    return error;
+  }
+};
+
+export const fetchBookApi = () => async (dispatch) => {
+  try {
+    const { data } = await Axios.get('https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/oF2DlchTxHbEjx4GHO3I/books');
+    //  dispatch(fetchBook(formData));
+
+    const Books = Object.keys(data).map((key) => ({
+      ...data[key][0],
+      item_id: key,
+    }));
+
+    const payload = Object.values(Books);
+    dispatch(fetchBook(payload));
+  } catch (error) {
+    return error;
+  }
+};
+
+export const removeBookApi = (payload) => async (dispatch) => {
+  try {
+    await Axios.delete(`https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/oF2DlchTxHbEjx4GHO3I/books/${payload}`);
+    dispatch(removeBook(payload));
+  } catch (error) {
+    return error;
+  }
+};
 // initialize State
 
 const initialState = [];
@@ -22,7 +66,10 @@ const reducer = (state = initialState, action) => {
     case ADD_BOOK:
       return [...state, action.payload];
     case REMOVE_BOOK:
-      return state.filter((book) => book.id !== action.payload);
+      return state.filter((book) => book.item_id !== action.payload);
+
+    case FETCH_BOOK:
+      return [...state, ...action.payload];
 
     default:
       return state;
